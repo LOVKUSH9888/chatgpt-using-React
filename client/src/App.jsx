9ccc4e707d87a8cac2e68002cd17bd48d9e14679
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-15 22:21:50 +0530

    Learnt the - JS template literals + the objects in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,81 @@
-import React from 'react'
-import send from './assets/send.svg'
-import user from './assets/user.png'
+import React from "react";
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import bot from "./assets/bot.png";
+import { useState } from "react";
+import loadingIcon from "./assets/loader.svg";
 
 const App = () => {
+  const [input, setInput] = ("");
+  const [post, setPost] = ([]);
+
+
+  const onSubmit = () => {
+    if(input.trim() === "") return;
+    updatePosts(input)
+  }
+
+  const updatePosts = (post) => {
+    setPosts(prevState => {
+      return [
+        ...prevState,
+        {type: "user", post}
+      ]
+    })
+  }
+
+  const onKeyUp = () => {
+
+  }
+
   return (
     <div>
-      <main className='chatGPT-app'>
-      <section className="chat-container">
-      <div className="layout">
-        <div className="chat-bubble">
-          <div className="avatar">
-            <img src={user}/>
+      <main className="chatGPT-app">
+        <section className="chat-container">
+          <div className="layout">
+            {posts.map((post, index) => (
+              <div
+                key={index}
+                className={`chat-bubble ${
+                  post.type === "bot" || post.type === "loading" ? "bot" : ""
+                }`}
+              >
+                <div className="avatar">
+                  <img
+                    src={
+                      post.type === "bot" || post.type === "loading"
+                        ? bot
+                        : user
+                    }
+                  />
+                </div>
+                {post.type === "loading" ? (
+                  <div className="loader">
+                    <img src={loadingIcon} />
+                  </div>
+                ) : (
+                  <div className="post">{post.post}</div>
+                )}
+              </div>
+            ))}
           </div>
-          <div className="post">Lorem, ipsum dolor.</div>
-        </div>
-      </div>
-      </section>
-      <footer>
-        <input className='composebar'
-        autoFocus
-        type='text'
-        placeholder='Ask Anything'
-        onChange={()=> {}} />
-        <div className="send-button">
-          <img src= {send} />
-        </div>
-      </footer>
-
+        </section>
+        <footer>
+          <input
+            className="composebar"
+            autoFocus
+            type="text"
+            placeholder="Ask Anything"
+            onChange={() => {(e)=> setInput(e.target.value)}}
+            onKeyUp={onKeyUp}
+          />
+          <div className="send-button" onClick={onSubmit}>
+            <img src={send} />
+          </div>
+        </footer>
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
